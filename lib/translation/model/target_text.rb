b799997715ba1dac49b993b6c5bda37d1e855f3b
philomagi <tooppoo56@gmail.com>
2021-06-20 03:12:23 +0900

    比較実装

--- lib/translation/model/target_text.rb
+++ lib/translation/model/target_text.rb
@@ -4,14 +4,26 @@ module Translation
   module Model
     # :section: 訳文
     class TargetText
+      attr_reader :sections
+
       def initialize(sections:)
         @sections = sections
       end
 
+      def ==(other)
+        @sections == other.sections
+      end
+
       class Section
+        attr_reader :source, :target
+
         def initialize(source:, target:)
           @source = source
           @target = target
+        end
+
+        def ==(other)
+          @source == other.source && @target == other.target
         end
       end
     end
