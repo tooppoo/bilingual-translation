721194dc6e5a13658095e091aa970d5aad3ea156
philomagi <tooppoo56@gmail.com>
2021-06-20 02:48:54 +0900

    module 追加

--- lib/translation/model/language.rb
+++ lib/translation/model/language.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
 module Translation
-  module Language
-    module Supported
-      English = "EN"
-      Japanese = "JA"
-    end
+  module Model
+    module Language
+      module Supported
+        English = "EN"
+        Japanese = "JA"
+      end
 
-    class LanguagePair
-      attr_reader :source, :target
+      class LanguagePair
+        attr_reader :source, :target
 
-      # @param [Symbol] source ソース言語
-      # @param [Symbol] target ターゲット言語
-      def initialize(source:, target:)
-        @source = source
-        @target = target
+        # @param [Symbol] source ソース言語
+        # @param [Symbol] target ターゲット言語
+        def initialize(source:, target:)
+          @source = source
+          @target = target
+        end
       end
     end
   end
