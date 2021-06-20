15909dbad211898bb78b4cc6aa5f554e972f9b1e
philomagi <tooppoo56@gmail.com>
2021-06-20 12:15:06 +0900

    rspec warning修正

--- spec/lib/translation/infrastructure/driver/deepl_spec.rb
+++ spec/lib/translation/infrastructure/driver/deepl_spec.rb
@@ -4,23 +4,21 @@ require "translation/infrastructure"
 require "translation/model"
 
 describe Translation::Infrastructure::Driver::DeepL do
-  Model = Translation::Model
-
   # ローカルでの動作確認用. 外部通信が発生するので、CIでは実行しない.
   xit "translate sample text" do
     auth_key = "REPLACE_BY_YOUR_AUTH_KEY_AT_RUNTIME_AND_MUST_NOT_COMMIT"
 
     client = Translation::Infrastructure::Driver::DeepL.free_plan(auth_key: auth_key)
 
-    source = Model::SourceText.new([
+    source = Translation::Model::SourceText.new([
                                      "Hello, Good Morning.",
                                      "My name is Jiro.",
                                    ])
 
     sut = client.translate source, from: "en", to: "ja"
-    expected = Model::TargetText.new(sections: [
-      Model::TargetText::Section.new(source: "Hello, Good Morning.", target: "こんにちは、おはようございます。"),
-      Model::TargetText::Section.new(source: "My name is Jiro.", target: "私の名前はJiroです。"),
+    expected = Translation::Model::TargetText.new(sections: [
+      Translation::Model::TargetText::Section.new(source: "Hello, Good Morning.", target: "こんにちは、おはようございます。"),
+      Translation::Model::TargetText::Section.new(source: "My name is Jiro.", target: "私の名前はJiroです。"),
     ])
 
     expect(sut).to eq expected
--- spec/lib/translation/model/service/translator_spec.rb
+++ spec/lib/translation/model/service/translator_spec.rb
@@ -2,41 +2,37 @@
 
 require "translation/model"
 
-Model = Translation::Model
-describe Model::Service::Translator do
-  let(:src_txt) { Model::SourceText.new(%w[text1 text2]) }
-
-  let(:formatter) do
-    Struct.new("MockFormatter") do
-      def format(source:, target:)
-        "#{source} -> #{target}"
-      end
+describe Translation::Model::Service::Translator do
+  Formatter = Struct.new("MockFormatter") do
+    def format(source:, target:)
+      "#{source} -> #{target}"
     end
   end
+  let(:src_txt) { Translation::Model::SourceText.new(%w[text1 text2]) }
 
   subject(:run_translator_service) do
-    sut = Model::Service::Translator.new(driver: driver)
-    input = Model::Service::Translator::Input.new(
+    sut = Translation::Model::Service::Translator.new(driver: driver)
+    input = Translation::Model::Service::Translator::Input.new(
       source: src_txt,
-      language_pair: Model::Language::LanguagePair.new(source: src_lang, target: target_lang),
-      formatter: formatter.new
+      language_pair: Translation::Model::Language::LanguagePair.new(source: src_lang, target: target_lang),
+      formatter: Formatter.new
     )
 
     sut.run(input)
   end
 
   context "language is supported" do
-    let(:src_lang) { Model::Language::Supported::English }
-    let(:target_lang) { Model::Language::Supported::Japanese }
+    let(:src_lang) { Translation::Model::Language::Supported::English }
+    let(:target_lang) { Translation::Model::Language::Supported::Japanese }
 
     let(:driver) do
       driver_mock = double("driver")
       expect(driver_mock).to receive("translate").with(src_txt, from: src_lang, to: target_lang) do |src|
         source_sentences = src.sentences
 
-        Model::TargetText.new(sections: [
-          Model::TargetText::Section.new(source: source_sentences[0], target: "文書1"),
-          Model::TargetText::Section.new(source: source_sentences[1], target: "文書2"),
+        Translation::Model::TargetText.new(sections: [
+          Translation::Model::TargetText::Section.new(source: source_sentences[0], target: "文書1"),
+          Translation::Model::TargetText::Section.new(source: source_sentences[1], target: "文書2"),
         ])
       end
 
@@ -56,18 +52,18 @@ describe Model::Service::Translator do
 
     context "source not supported" do
       let(:src_lang) { "UNKNOWN" }
-      let(:target_lang) { Model::Language::Supported::Japanese }
+      let(:target_lang) { Translation::Model::Language::Supported::Japanese }
 
       it "should raise error" do
-        expect { run_translator_service }.to raise_error(Model::Language::UnsupportedError)
+        expect { run_translator_service }.to raise_error(Translation::Model::Language::UnsupportedError)
       end
     end
     context "target not supported" do
-      let(:src_lang) { Model::Language::Supported::English }
+      let(:src_lang) { Translation::Model::Language::Supported::English }
       let(:target_lang) { "UNKNOWN" }
 
       it "should raise error" do
-        expect { run_translator_service }.to raise_error(Model::Language::UnsupportedError)
+        expect { run_translator_service }.to raise_error(Translation::Model::Language::UnsupportedError)
       end
     end
     context "both of source and target not supported" do
@@ -75,7 +71,7 @@ describe Model::Service::Translator do
       let(:target_lang) { "UNKNOWN" }
 
       it "should raise error" do
-        expect { run_translator_service }.to raise_error(Model::Language::UnsupportedError)
+        expect { run_translator_service }.to raise_error(Translation::Model::Language::UnsupportedError)
       end
     end
   end
